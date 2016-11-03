8dc343f6fc9df05892a801361bb0c20f38685e4b
Amritanshu Agrawal <aagrawa8@ncsu.edu>
2016-11-03 17:21:34 -0400

    preprocessing

--- src/SE_preprocess.py
+++ src/SE_preprocess.py
@@ -4,7 +4,7 @@ __author__ = 'amrit'
 import csv
 from Preprocess import *
 import sys
-
+#3370528
 sys.dont_write_bytecode = True
 
 if __name__ == "__main__":
@@ -12,18 +12,18 @@ if __name__ == "__main__":
     features = []
     with open('/Users/amrit/Downloads/train.csv') as csvfile:
         reader = csv.DictReader(csvfile)
-        headers = reader.fieldnames[:6]+['Text']+reader.fieldnames[13:]
+        headers = reader.fieldnames
         with open('train.csv', 'w') as csvfile1:
             fieldnames = ['first_name', 'last_name']
             writer = csv.DictWriter(csvfile1, fieldnames=headers)
             writer.writeheader()
-            for row in reader:
+            for i,row in enumerate(reader):
                 str=row['Title']+' ' + row['BodyMarkdown']+' ' +row['Tag1']+' '+row['Tag2']+' '+row['Tag3']+' '+ row['Tag4']+' '+row['Tag5']
                 line = process(str, string_lower, email_urls, unicode_normalisation, punctuate_preproc,
                                    numeric_isolation, stopwords, stemming, word_len_less)
                 dic={}
                 for x in headers:
-                    if x=='Text':
+                    if x=='BodyMarkdown':
                         dic[x]=line
                     else:
                         dic[x]=row[x]
